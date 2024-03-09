2b4bd929190eca2c437f9f42d6ce0ee5e50d4b78
Ankur Sarkar <sarkarrohonankur@gmail.com>
2024-03-09 08:44:10 +0530

    update

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,16 +9,16 @@ const Header = () => {
     >
       <div className="w-full h-2/4 md:h-full md:w-2/5 flex flex-col justify-center items-center md:items-start gap-8">
         <div className="flex flex-col gap-2">
-          <h1 className="text-4xl font-black md:text-8xl">Insightful</h1>
+          <h1 className="text-4xl font-black md:text-8xl">Empower U</h1>
           <h2 className="text-md md:text-2xl">Start growing today!</h2>
         </div>
         <p className="max-w-md text-sm md:text-base text-zinc-500">
-          Insightful is an AI-powered sales optimization tool that provides
-          data-driven insights to boost sales performance.
+          Empower U is an AI-powered Fundraising optimization tool that provides
+          data-driven insights to boost your Project.
         </p>
         <div className="w-full flex items-center justify-center md:justify-start gap-4">
           <button className="w-48 h-12 text-sm sm:text-base rounded bg-white text-black hover:bg-fuchsia-700 hover:text-white transition-colors">
-            Try 7 days free!
+            Get Started!
           </button>
           <button className="w-48 h-12 text-sm sm:text-base rounded hover:bg-white hover:text-white hover:bg-opacity-5 transition-colors">
             Contact
